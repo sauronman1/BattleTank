07274fcf82c69b39c64cbd6fe90cc4363e8d7cb8
DESKTOP-9B6JMC2\aboud <aboud-malki@hotmail.com>
2019-01-03 21:27:46 +0100

    MA12: Currently working on GetLookVectorHitLocation() that will return worldposition of the part of the world that linetrace hits

--- MetalAbyss/Source/MetalAbyss/Private/TankPlayerController.cpp
+++ MetalAbyss/Source/MetalAbyss/Private/TankPlayerController.cpp
@@ -2,6 +2,7 @@
 
 #include "TankPlayerController.h"
 #include "Tank.h"
+#include "Engine/World.h"
 
 void ATankPlayerController::BeginPlay()
 {
@@ -10,11 +11,17 @@ void ATankPlayerController::BeginPlay()
 	ATank* Tank = GetControlledTank();
 	if (!Tank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Player has not possessed a %s"));
+		UE_LOG(LogTemp, Warning, TEXT("Player has not possessed a %s"), *Tank->GetName());
 	}
 	else{
 		UE_LOG(LogTemp, Warning, TEXT("Player has possessed a %s"), *Tank->GetName());
 	}
+}
+
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
 }
 
 ATank * ATankPlayerController::GetControlledTank() const
@@ -23,3 +30,61 @@ ATank * ATankPlayerController::GetControlledTank() const
 }
 
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; // Out parameter
+	if (GetSightRayHitLocation(HitLocation)) // Has "Side effect", is going to line trace
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+
+		// TODO Tell controlled tank to aim at this point
+	}
+	
+}
+
+// Get world location if linetrace through crosshair, true if it hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	// Find the crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrosshairXLocation, ViewportSizeX * CrosshairYLocation);
+	//UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *ScreenLocation.ToString()); // TODO May not print accurate location, review it
+    // "De-project"  the screen position of the crosshair to a world direction
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), *LookDirection.ToString());
+		GetLookVectorHitLocation(LookDirection, HitLocation);
+	}
+	// Line-trace along that look direction, and see what we hit (up to max range)
+	return true;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CameraWorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, CameraWorldLocation, LookDirection);
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
+{
+
+	const static FName TraceTag = TEXT("TraceTag");
+	GetWorld()->DebugDrawTraceTag = TraceTag;
+	FCollisionQueryParams Params(TraceTag);
+
+	FHitResult HitResult;
+	auto StartLocation = PlayerCameraManager->GetCameraLocation();
+	auto EndLocation = StartLocation + (LookDirection * LineTraceRange);
+	if(GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECollisionChannel::ECC_Camera, Params)) //ECC_Visibility tells us to hit anything that is visible
+	{
+		HitLocation = HitResult.Location;
+		UE_LOG(LogTemp, Warning, TEXT("F***ING WORK! %s"), *HitResult.Location.ToString());
+		return true;
+	}
+	HitLocation = FVector(0);
+	return false;
+}
--- MetalAbyss/Source/MetalAbyss/Public/TankPlayerController.h
+++ MetalAbyss/Source/MetalAbyss/Public/TankPlayerController.h
@@ -15,10 +15,26 @@ class METALABYSS_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 
 private:
 
 	ATank* GetControlledTank() const;
-	virtual void BeginPlay() override;
 	
+	// Makes the tank start moving the barrel so that the shot would hit where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	// return an OUT parameter, true if it hits landscape
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& HitLocation) const;
+	UPROPERTY(EditAnywhere)
+		float LineTraceRange = 1000000;
+	UPROPERTY(EditAnywhere)
+		float CrosshairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+		float CrosshairYLocation = 0.3333;
+
 };
